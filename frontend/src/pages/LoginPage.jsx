f3aa996eeef818244aff0af98676b868ff5ae13d
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-01-13 17:51:11 -0500

    Added login page

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      const response = await fetch('/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        navigate('/');
+        console.log('Login successful', data);
+      } else {
+        throw new Error(data.data || 'Error logging in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      {errorMessage && <p>{errorMessage}</p>}
+      <input
+        type="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
